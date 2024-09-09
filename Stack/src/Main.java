790c2b887d84e60f0d800fbe57803935adaed5da
OMAR ABARRA <abarraomar@gmail.com>
2024-09-09 14:38:32 +0100

    add Pop method

--- Stack/src/Main.java
+++ Stack/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -16,5 +18,51 @@ public class Main {
         newstack.Pop();
 
         newstack.printStack();
-    }
-}
+
+
+
+        class Stack<T> {
+
+            private ArrayList<T> stackList = new ArrayList<>();
+
+            public ArrayList<T> getStackList() {
+                return stackList;
+            }
+
+            public void printStack() {
+                for (int i = stackList.size() - 1; i >= 0; i--) {
+                    System.out.println(stackList.get(i));
+                }
+            }
+
+            public boolean isEmpty() {
+                return stackList.size() == 0;
+            }
+
+            public T peek() {
+                if (isEmpty()) {
+                    return null;
+                } else {
+                    return stackList.get(stackList.size() - 1);
+                }
+            }
+
+            public int size() {
+                return stackList.size();
+            }
+
+            public void push(T value) {
+                stackList.add(value);
+            }
+
+            public T pop() {
+                if (isEmpty()) {
+                    return null;
+                } else {
+                    return stackList.remove(stackList.size() - 1);
+                }
+            }
+
+        }
+    }}
+
