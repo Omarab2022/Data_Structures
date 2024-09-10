ecf2d3046fb585b751d38ce92ea8e89094e02ac8
OMAR ABARRA <abarraomar@gmail.com>
2024-09-10 12:19:01 +0100

    add tree folder

---
+++ Tree/src/BinarySearchTree.java
@@ -0,0 +1,23 @@
+
+
+public class BinarySearchTree {
+
+    class Node{
+        int value ;
+        Node left;
+        Node right;
+
+        public Node(int value ){
+            this.value=value;
+        }
+    }
+
+    Node root;
+
+    public BinarySearchTree(){
+        this.root=null;
+    }
+
+
+
+}
---
+++ Tree/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+
+        System.out.println("root of this tree is : " + binarySearchTree.root);
+    }
+}
